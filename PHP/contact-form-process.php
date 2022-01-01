676eb14a4d1320466a5b5ac7fc922a092a3efe39
Elias <eliasheggernes@gmail.com>
2022-01-01 18:08:15 +0100

    contact update fix

--- PHP/contact-form-process.php
+++ PHP/contact-form-process.php
@@ -71,7 +71,7 @@ if (isset($_POST['Email'])) {
 <head>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title>Takk for at du kontaktet oss</title>
+<title>Takk for at du kontaktet Multicore</title>
 <script src="https://kit.fontawesome.com/911551dd30.js" crossorigin="anonymous"></script>
 <link rel="shortcut icon" type="image/x-icon" href="../logo/favicon.ico">
 <link href="../CSS/my-style.css" rel="stylesheet" type="text/css">
@@ -82,8 +82,8 @@ if (isset($_POST['Email'])) {
 
 <span id="menu-preset"></span>
 			
-<div id="contact-form-thanks" style="background-color: #2E3B56; color: black; padding: 300px 0px;">
- <p style="text-align: center;">Error. Send mail til eliasheggernes@gmail.com</p>
+<div id="contact-form-thanks" style="background-color: #2E3B56; color: #EDEFFB; padding: 300px 0px;">
+ <p style="text-align: center;">Takk for at du kontaktet Multicore</p>
 </div>
 	
 <span id="end-preset"></span>
