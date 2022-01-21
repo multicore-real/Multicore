9f5e1265d44323a501588a69fc45922864cb4d15
Elias <eliasheggernes@gmail.com>
2022-01-21 13:18:38 +0100

    slide effekt paa tekst

--- Java-Script/style.js
+++ Java-Script/style.js
@@ -7,6 +7,17 @@ function closeNav() {
 }
 
 function myFunction() {
-    document.getElementById("video").scrollIntoView();
-    
+    document.getElementById("video").scrollIntoView({behavior: "smooth"});
+}
+
+function Bilder() {
+    document.getElementById("referansebidler").scrollIntoView({behavior: "smooth"});
+}
+
+function produkter() {
+    document.getElementById("product-con").scrollIntoView({behavior: "smooth"});
+}
+
+function grafisk_design(){
+    document.getElementById("graphic-con").scrollIntoView({behavior: "smooth"});
 }
